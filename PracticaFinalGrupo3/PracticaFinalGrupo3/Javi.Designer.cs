b41a17d1666c9d462ac39fd588f97f505e13c059
javiloza <javier.jlm.lozanomartin@gmail.com>
2014-05-22 20:42:16 +0200

    prueba a poner un cambio
    he puesto un boton

--- PracticaFinalGrupo3/PracticaFinalGrupo3/Javi.Designer.cs
+++ PracticaFinalGrupo3/PracticaFinalGrupo3/Javi.Designer.cs
@@ -28,11 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(313, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 175);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Javi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 560);
+            this.Controls.Add(this.button1);
+            this.Name = "Javi";
             this.Text = "Javi";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
