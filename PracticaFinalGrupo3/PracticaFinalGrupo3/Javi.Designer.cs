ab3a8e2cb1aef537f925924ea3ea2ba880b162b9
javiloza <javier.jlm.lozanomartin@gmail.com>
2014-05-29 19:25:42 +0200

    he terrminado el proyecto
    finito

--- PracticaFinalGrupo3/PracticaFinalGrupo3/Javi.Designer.cs
+++ PracticaFinalGrupo3/PracticaFinalGrupo3/Javi.Designer.cs
@@ -28,11 +28,378 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Pantalla = new System.Windows.Forms.TextBox();
+            this.num1 = new System.Windows.Forms.Button();
+            this.num2 = new System.Windows.Forms.Button();
+            this.num3 = new System.Windows.Forms.Button();
+            this.num4 = new System.Windows.Forms.Button();
+            this.num5 = new System.Windows.Forms.Button();
+            this.num6 = new System.Windows.Forms.Button();
+            this.num7 = new System.Windows.Forms.Button();
+            this.num8 = new System.Windows.Forms.Button();
+            this.num9 = new System.Windows.Forms.Button();
+            this.num0 = new System.Windows.Forms.Button();
+            this.punto = new System.Windows.Forms.Button();
+            this.atras = new System.Windows.Forms.Button();
+            this.borrar = new System.Windows.Forms.Button();
+            this.multiplicacion = new System.Windows.Forms.Button();
+            this.cuadrado = new System.Windows.Forms.Button();
+            this.division = new System.Windows.Forms.Button();
+            this.raiz = new System.Windows.Forms.Button();
+            this.resta = new System.Windows.Forms.Button();
+            this.suma = new System.Windows.Forms.Button();
+            this.igual = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(324, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(96, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // Pantalla
+            // 
+            this.Pantalla.Enabled = false;
+            this.Pantalla.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pantalla.Location = new System.Drawing.Point(9, 43);
+            this.Pantalla.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Pantalla.Multiline = true;
+            this.Pantalla.Name = "Pantalla";
+            this.Pantalla.ReadOnly = true;
+            this.Pantalla.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Pantalla.Size = new System.Drawing.Size(302, 80);
+            this.Pantalla.TabIndex = 1;
+            this.Pantalla.Text = "0";
+            // 
+            // num1
+            // 
+            this.num1.BackColor = System.Drawing.Color.Lime;
+            this.num1.Location = new System.Drawing.Point(10, 318);
+            this.num1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(56, 52);
+            this.num1.TabIndex = 2;
+            this.num1.Text = "1";
+            this.num1.UseVisualStyleBackColor = false;
+            this.num1.Click += new System.EventHandler(this.num1_Click);
+            // 
+            // num2
+            // 
+            this.num2.BackColor = System.Drawing.Color.Lime;
+            this.num2.Location = new System.Drawing.Point(70, 318);
+            this.num2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(56, 52);
+            this.num2.TabIndex = 3;
+            this.num2.Text = "2";
+            this.num2.UseVisualStyleBackColor = false;
+            this.num2.Click += new System.EventHandler(this.num2_Click);
+            // 
+            // num3
+            // 
+            this.num3.BackColor = System.Drawing.Color.Lime;
+            this.num3.Location = new System.Drawing.Point(131, 318);
+            this.num3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.num3.Name = "num3";
+            this.num3.Size = new System.Drawing.Size(56, 52);
+            this.num3.TabIndex = 4;
+            this.num3.Text = "3";
+            this.num3.UseVisualStyleBackColor = false;
+            this.num3.Click += new System.EventHandler(this.num3_Click);
+            // 
+            // num4
+            // 
+            this.num4.BackColor = System.Drawing.Color.Lime;
+            this.num4.Location = new System.Drawing.Point(9, 261);
+            this.num4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.num4.Name = "num4";
+            this.num4.Size = new System.Drawing.Size(56, 52);
+            this.num4.TabIndex = 5;
+            this.num4.Text = "4";
+            this.num4.UseVisualStyleBackColor = false;
+            this.num4.Click += new System.EventHandler(this.num4_Click);
+            // 
+            // num5
+            // 
+            this.num5.BackColor = System.Drawing.Color.Lime;
+            this.num5.Location = new System.Drawing.Point(70, 261);
+            this.num5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.num5.Name = "num5";
+            this.num5.Size = new System.Drawing.Size(56, 52);
+            this.num5.TabIndex = 6;
+            this.num5.Text = "5";
+            this.num5.UseVisualStyleBackColor = false;
+            this.num5.Click += new System.EventHandler(this.num5_Click);
+            // 
+            // num6
+            // 
+            this.num6.BackColor = System.Drawing.Color.Lime;
+            this.num6.Location = new System.Drawing.Point(131, 261);
+            this.num6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.num6.Name = "num6";
+            this.num6.Size = new System.Drawing.Size(56, 52);
+            this.num6.TabIndex = 7;
+            this.num6.Text = "6";
+            this.num6.UseVisualStyleBackColor = false;
+            this.num6.Click += new System.EventHandler(this.num6_Click);
+            // 
+            // num7
+            // 
+            this.num7.BackColor = System.Drawing.Color.Lime;
+            this.num7.Location = new System.Drawing.Point(9, 204);
+            this.num7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.num7.Name = "num7";
+            this.num7.Size = new System.Drawing.Size(56, 52);
+            this.num7.TabIndex = 8;
+            this.num7.Text = "7";
+            this.num7.UseVisualStyleBackColor = false;
+            this.num7.Click += new System.EventHandler(this.num7_Click);
+            // 
+            // num8
+            // 
+            this.num8.BackColor = System.Drawing.Color.Lime;
+            this.num8.Location = new System.Drawing.Point(70, 204);
+            this.num8.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.num8.Name = "num8";
+            this.num8.Size = new System.Drawing.Size(56, 52);
+            this.num8.TabIndex = 9;
+            this.num8.Text = "8";
+            this.num8.UseVisualStyleBackColor = false;
+            this.num8.Click += new System.EventHandler(this.num8_Click);
+            // 
+            // num9
+            // 
+            this.num9.BackColor = System.Drawing.Color.Lime;
+            this.num9.Location = new System.Drawing.Point(131, 204);
+            this.num9.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.num9.Name = "num9";
+            this.num9.Size = new System.Drawing.Size(56, 52);
+            this.num9.TabIndex = 10;
+            this.num9.Text = "9";
+            this.num9.UseVisualStyleBackColor = false;
+            this.num9.Click += new System.EventHandler(this.num9_Click);
+            // 
+            // num0
+            // 
+            this.num0.BackColor = System.Drawing.Color.Lime;
+            this.num0.Location = new System.Drawing.Point(10, 375);
+            this.num0.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.num0.Name = "num0";
+            this.num0.Size = new System.Drawing.Size(117, 52);
+            this.num0.TabIndex = 11;
+            this.num0.Text = "0";
+            this.num0.UseVisualStyleBackColor = false;
+            this.num0.Click += new System.EventHandler(this.num0_Click);
+            // 
+            // punto
+            // 
+            this.punto.BackColor = System.Drawing.Color.Lime;
+            this.punto.Location = new System.Drawing.Point(131, 375);
+            this.punto.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.punto.Name = "punto";
+            this.punto.Size = new System.Drawing.Size(56, 52);
+            this.punto.TabIndex = 12;
+            this.punto.Text = ".";
+            this.punto.UseVisualStyleBackColor = false;
+            // 
+            // atras
+            // 
+            this.atras.BackColor = System.Drawing.Color.Lime;
+            this.atras.Location = new System.Drawing.Point(9, 148);
+            this.atras.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.atras.Name = "atras";
+            this.atras.Size = new System.Drawing.Size(117, 52);
+            this.atras.TabIndex = 13;
+            this.atras.Text = "Deshacer";
+            this.atras.UseVisualStyleBackColor = false;
+            this.atras.Click += new System.EventHandler(this.atras_Click);
+            // 
+            // borrar
+            // 
+            this.borrar.BackColor = System.Drawing.Color.Lime;
+            this.borrar.Location = new System.Drawing.Point(130, 148);
+            this.borrar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.borrar.Name = "borrar";
+            this.borrar.Size = new System.Drawing.Size(57, 52);
+            this.borrar.TabIndex = 14;
+            this.borrar.Text = "C";
+            this.borrar.UseVisualStyleBackColor = false;
+            this.borrar.Click += new System.EventHandler(this.borrar_Click);
+            // 
+            // multiplicacion
+            // 
+            this.multiplicacion.BackColor = System.Drawing.Color.Lime;
+            this.multiplicacion.Location = new System.Drawing.Point(192, 148);
+            this.multiplicacion.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.multiplicacion.Name = "multiplicacion";
+            this.multiplicacion.Size = new System.Drawing.Size(57, 52);
+            this.multiplicacion.TabIndex = 15;
+            this.multiplicacion.Text = "X";
+            this.multiplicacion.UseVisualStyleBackColor = false;
+            this.multiplicacion.Click += new System.EventHandler(this.multiplicacion_Click);
+            // 
+            // cuadrado
+            // 
+            this.cuadrado.BackColor = System.Drawing.Color.Lime;
+            this.cuadrado.Location = new System.Drawing.Point(254, 148);
+            this.cuadrado.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cuadrado.Name = "cuadrado";
+            this.cuadrado.Size = new System.Drawing.Size(57, 52);
+            this.cuadrado.TabIndex = 16;
+            this.cuadrado.Text = "X^2";
+            this.cuadrado.UseVisualStyleBackColor = false;
+            this.cuadrado.Click += new System.EventHandler(this.cuadrado_Click);
+            // 
+            // division
+            // 
+            this.division.BackColor = System.Drawing.Color.Lime;
+            this.division.Location = new System.Drawing.Point(192, 204);
+            this.division.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.division.Name = "division";
+            this.division.Size = new System.Drawing.Size(57, 52);
+            this.division.TabIndex = 17;
+            this.division.Text = "/";
+            this.division.UseVisualStyleBackColor = false;
+            this.division.Click += new System.EventHandler(this.division_Click);
+            // 
+            // raiz
+            // 
+            this.raiz.BackColor = System.Drawing.Color.Lime;
+            this.raiz.Location = new System.Drawing.Point(254, 204);
+            this.raiz.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.raiz.Name = "raiz";
+            this.raiz.Size = new System.Drawing.Size(57, 52);
+            this.raiz.TabIndex = 18;
+            this.raiz.Text = "RAIZ";
+            this.raiz.UseVisualStyleBackColor = false;
+            this.raiz.Click += new System.EventHandler(this.raiz_Click);
+            // 
+            // resta
+            // 
+            this.resta.BackColor = System.Drawing.Color.Lime;
+            this.resta.Location = new System.Drawing.Point(192, 261);
+            this.resta.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.resta.Name = "resta";
+            this.resta.Size = new System.Drawing.Size(57, 52);
+            this.resta.TabIndex = 19;
+            this.resta.Text = "-";
+            this.resta.UseVisualStyleBackColor = false;
+            this.resta.Click += new System.EventHandler(this.resta_Click);
+            // 
+            // suma
+            // 
+            this.suma.BackColor = System.Drawing.Color.Lime;
+            this.suma.Location = new System.Drawing.Point(192, 318);
+            this.suma.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.suma.Name = "suma";
+            this.suma.Size = new System.Drawing.Size(57, 109);
+            this.suma.TabIndex = 20;
+            this.suma.Text = "+";
+            this.suma.UseVisualStyleBackColor = false;
+            this.suma.Click += new System.EventHandler(this.suma_Click);
+            // 
+            // igual
+            // 
+            this.igual.BackColor = System.Drawing.Color.Lime;
+            this.igual.Location = new System.Drawing.Point(254, 261);
+            this.igual.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.igual.Name = "igual";
+            this.igual.Size = new System.Drawing.Size(57, 166);
+            this.igual.TabIndex = 21;
+            this.igual.Text = "=";
+            this.igual.UseVisualStyleBackColor = false;
+            this.igual.Click += new System.EventHandler(this.igual_Click);
+            // 
+            // Javi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(324, 462);
+            this.Controls.Add(this.igual);
+            this.Controls.Add(this.suma);
+            this.Controls.Add(this.resta);
+            this.Controls.Add(this.raiz);
+            this.Controls.Add(this.division);
+            this.Controls.Add(this.cuadrado);
+            this.Controls.Add(this.multiplicacion);
+            this.Controls.Add(this.borrar);
+            this.Controls.Add(this.atras);
+            this.Controls.Add(this.punto);
+            this.Controls.Add(this.num0);
+            this.Controls.Add(this.num9);
+            this.Controls.Add(this.num8);
+            this.Controls.Add(this.num7);
+            this.Controls.Add(this.num6);
+            this.Controls.Add(this.num5);
+            this.Controls.Add(this.num4);
+            this.Controls.Add(this.num3);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
+            this.Controls.Add(this.Pantalla);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Javi";
             this.Text = "Javi";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.TextBox Pantalla;
+        private System.Windows.Forms.Button num1;
+        private System.Windows.Forms.Button num2;
+        private System.Windows.Forms.Button num3;
+        private System.Windows.Forms.Button num4;
+        private System.Windows.Forms.Button num5;
+        private System.Windows.Forms.Button num6;
+        private System.Windows.Forms.Button num7;
+        private System.Windows.Forms.Button num8;
+        private System.Windows.Forms.Button num9;
+        private System.Windows.Forms.Button num0;
+        private System.Windows.Forms.Button punto;
+        private System.Windows.Forms.Button atras;
+        private System.Windows.Forms.Button borrar;
+        private System.Windows.Forms.Button multiplicacion;
+        private System.Windows.Forms.Button cuadrado;
+        private System.Windows.Forms.Button division;
+        private System.Windows.Forms.Button raiz;
+        private System.Windows.Forms.Button resta;
+        private System.Windows.Forms.Button suma;
+        private System.Windows.Forms.Button igual;
+
     }
 }
--- PracticaFinalGrupo3/PracticaFinalGrupo3/Javi.cs
+++ PracticaFinalGrupo3/PracticaFinalGrupo3/Javi.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +18,269 @@ namespace PracticaFinalGrupo3
         {
             InitializeComponent();
         }
+        bool limpiaPantalla = true;
+        String operacion, borrado;
+        double numero1, numero2, resultado;
+
+
+
+        //Aqui definimos los numeros
+        private void num1_Click(object sender, EventArgs e)
+        {
+            if (limpiaPantalla == true)
+            {
+                Pantalla.Text = "";
+                Pantalla.Text = "1";
+                limpiaPantalla = false;
+            }
+            else
+            {
+                Pantalla.Text = Pantalla.Text + "1";
+            }
+        }
+
+        private void num2_Click(object sender, EventArgs e)
+        {
+            if (limpiaPantalla == true)
+            {
+                Pantalla.Text = "";
+                Pantalla.Text = "2";
+                limpiaPantalla = false;
+            }
+            else
+            {
+                Pantalla.Text = Pantalla.Text + "2";
+            }
+        }
+
+        private void num3_Click(object sender, EventArgs e)
+        {
+            if (limpiaPantalla == true)
+            {
+                Pantalla.Text = "";
+                Pantalla.Text = "3";
+                limpiaPantalla = false;
+            }
+            else
+            {
+                Pantalla.Text = Pantalla.Text + "3";
+            }
+        }
+
+        private void num4_Click(object sender, EventArgs e)
+        {
+            if (limpiaPantalla == true)
+            {
+                Pantalla.Text = "";
+                Pantalla.Text = "4";
+                limpiaPantalla = false;
+            }
+            else
+            {
+                Pantalla.Text = Pantalla.Text + "4";
+            }
+        }
+
+        private void num5_Click(object sender, EventArgs e)
+        {
+            if (limpiaPantalla == true)
+            {
+                Pantalla.Text = "";
+                Pantalla.Text = "5";
+                limpiaPantalla = false;
+            }
+            else
+            {
+                Pantalla.Text = Pantalla.Text + "5";
+            }
+        }
+
+        private void num6_Click(object sender, EventArgs e)
+        {
+            if (limpiaPantalla == true)
+            {
+                Pantalla.Text = "";
+                Pantalla.Text = "6";
+                limpiaPantalla = false;
+            }
+            else
+            {
+                Pantalla.Text = Pantalla.Text + "6";
+            }
+        }
+
+        private void num7_Click(object sender, EventArgs e)
+        {
+            if (limpiaPantalla == true)
+            {
+                Pantalla.Text = "";
+                Pantalla.Text = "7";
+                limpiaPantalla = false;
+            }
+            else
+            {
+                Pantalla.Text = Pantalla.Text + "7";
+            }
+        }
+
+        private void num8_Click(object sender, EventArgs e)
+        {
+            if (limpiaPantalla == true)
+            {
+                Pantalla.Text = "";
+                Pantalla.Text = "8";
+                limpiaPantalla = false;
+            }
+            else
+            {
+                Pantalla.Text = Pantalla.Text + "8";
+            }
+        }
+
+        private void num9_Click(object sender, EventArgs e)
+        {
+            if (limpiaPantalla == true)
+            {
+                Pantalla.Text = "";
+                Pantalla.Text = "9";
+                limpiaPantalla = false;
+            }
+            else
+            {
+                Pantalla.Text = Pantalla.Text + "9";
+            }
+        }
+
+        private void num0_Click(object sender, EventArgs e)
+        {
+            if (Pantalla.Text == "0")
+            {
+                return;
+            }
+            else
+            {
+                Pantalla.Text = Pantalla.Text + "0";
+            }
+
+        }
+         
+        // fin de los numeros
+
+        //inicio de los botones de las operaciones
+      private void suma_Click(object sender, EventArgs e)
+        {
+            operacion = "+";
+            numero1 = double.Parse(Pantalla.Text);
+            limpiaPantalla = true;
+        }
+
+      private void resta_Click(object sender, EventArgs e)
+      {
+          operacion = "-";
+          numero1 = double.Parse(Pantalla.Text);
+          limpiaPantalla = true;
+      }
+
+      private void multiplicacion_Click(object sender, EventArgs e)
+      {
+          operacion = "*";
+          numero1 = double.Parse(Pantalla.Text);
+          limpiaPantalla = true;
+      }
+
+      private void division_Click(object sender, EventArgs e)
+      {
+          operacion = "/";
+          numero1 = double.Parse(Pantalla.Text);
+          limpiaPantalla = true;
+      }
+
+      private void cuadrado_Click(object sender, EventArgs e)
+      {
+          numero1 = double.Parse(Pantalla.Text);
+          resultado = numero1 * numero1;
+          Pantalla.Text = resultado.ToString();
+          limpiaPantalla = true;
+      }
+
+
+      private void raiz_Click(object sender, EventArgs e)
+      {
+          numero1 = double.Parse(Pantalla.Text);
+          resultado = Math.Sqrt(numero1);
+          Pantalla.Text = resultado.ToString();
+          limpiaPantalla = true;
+      }
+
+      private void igual_Click(object sender, EventArgs e)
+      {
+          numero2 = double.Parse(Pantalla.Text);
+          if (operacion == "+")
+          {
+              resultado = numero1 + numero2;
+              Pantalla.Text = resultado.ToString();
+              limpiaPantalla = true;
+          }
+          if (operacion == "-")
+          {
+              resultado = numero1 - numero2;
+              Pantalla.Text = resultado.ToString();
+              limpiaPantalla = true;
+          }
+          if (operacion == "*")
+          {
+              resultado = numero1 * numero2;
+              Pantalla.Text = resultado.ToString();
+              limpiaPantalla = true;
+          }
+          if (operacion == "/")
+          {
+              resultado = numero1 / numero2;
+              Pantalla.Text = resultado.ToString();
+              limpiaPantalla = true;
+          }
+          // fin de los botones de operaciones
+
+          // botones especiles
+      }
+
+      private void atras_Click(object sender, EventArgs e)
+      {
+          int x = 0;
+          borrado = Pantalla.Text;
+          x = borrado.Length - 1;
+          borrado = borrado.Substring(0, x);
+          Pantalla.Text = borrado;
+
+
+          if (Pantalla.Text == "")
+          {
+              Pantalla.Text = "0";
+              limpiaPantalla = true;
+          }
+          if (Pantalla.Text == "-")
+          {
+              Pantalla.Text = "0";
+              limpiaPantalla = true;
+          }
+      }
+
+      private void borrar_Click(object sender, EventArgs e)
+      {
+          Pantalla.Text = "0";
+          numero1 = 0;
+          numero2 = 0;
+          limpiaPantalla = true;
+
+      }
+
+      private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+      {
+          this.Close();
+      }
+
+       
+
+
     }
 }
