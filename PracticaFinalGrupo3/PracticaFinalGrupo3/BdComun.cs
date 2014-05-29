6c1eb6b17cdb1a353c61bfb566e26da5d6438ff2
hooh125 <andreseduardomp@gmail.com>
2014-05-29 23:33:34 +0200

    Haciendo las clases mejor
    Mas entendibles

--- PracticaFinalGrupo3/PracticaFinalGrupo3/BdComun.cs
+++ PracticaFinalGrupo3/PracticaFinalGrupo3/BdComun.cs
@@ -22,17 +22,17 @@ namespace PracticaFinalGrupo3
                 conectar.Open();
                 return conectar;
         }
+
         public static void meteConsulta(DataGridView dgvControl, MySqlConnection conexion, string consulta)
         {
             DataSet dsConsulta;
             MySqlDataAdapter daConsulta;
-
             daConsulta = new MySqlDataAdapter(consulta, conexion);
-
             dsConsulta = new DataSet();
             daConsulta.Fill(dsConsulta, "consulta");
             dgvControl.DataSource = dsConsulta;
             dgvControl.DataMember = "consulta";
         }
+
     }
 }
