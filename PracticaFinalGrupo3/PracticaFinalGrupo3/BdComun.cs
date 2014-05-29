1976c8bd32c7ba9a1bbc63f13c786e4b6e3d7553
Emilio Moreno <emoord@gmail.com>
2014-05-29 22:42:38 +0200

    Recuperando el master

---
+++ PracticaFinalGrupo3/PracticaFinalGrupo3/BdComun.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+using MySql.Data;
+using System.Data;
+
+
+namespace PracticaFinalGrupo3
+{
+    public class BdComun
+    {
+
+        public static String datosConexion = "Server=127.0.0.1;User id=root;Database=biblioteca;password=;";
+
+        public static MySqlConnection obtenerConexion() {
+                MySqlConnection conectar = new MySqlConnection(datosConexion);
+                conectar.Open();
+                return conectar;
+        }
+        public static void meteConsulta(DataGridView dgvControl, MySqlConnection conexion, string consulta)
+        {
+            DataSet dsConsulta;
+            MySqlDataAdapter daConsulta;
+
+            daConsulta = new MySqlDataAdapter(consulta, conexion);
+
+            dsConsulta = new DataSet();
+            daConsulta.Fill(dsConsulta, "consulta");
+            dgvControl.DataSource = dsConsulta;
+            dgvControl.DataMember = "consulta";
+        }
+    }
+}
---
+++ PracticaFinalGrupo3/PracticaFinalGrupo3/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace PracticaFinalGrupo3
+{
+    public partial class Form1 : Form
+    {
+        String cadenaConexion = "Server=" + "localhost" + ";Port=" + "3306" + ";Database=" + "biblioteca" + ";Uid=" + "root" + ";Pwd=" + "" + ";";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public MySqlDataAdapter conectaALaBaseDeDatos(String query)
+        {
+            MySqlConnection conexion = new MySqlConnection();
+            conexion.ConnectionString = cadenaConexion;
+            try
+            {
+                conexion.Open();
+                MySqlDataAdapter adaptadorDatos = new MySqlDataAdapter(query, conexion);
+                return adaptadorDatos;
+            }
+            catch {
+                return null;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new Andres().ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            new Javi().ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            new Manu().ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            new Emilio().ShowDialog();
+        }
+    }
+}
