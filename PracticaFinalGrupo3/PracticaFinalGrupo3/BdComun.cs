6fecac6a3fca7482118ef04b096819c9d535f70e
hooh125 <andreseduardomp@gmail.com>
2014-05-30 15:59:50 +0200

    commit
    commit

--- PracticaFinalGrupo3/PracticaFinalGrupo3/BdComun.cs
+++ PracticaFinalGrupo3/PracticaFinalGrupo3/BdComun.cs
@@ -22,36 +22,8 @@ namespace PracticaFinalGrupo3
                 conectar.Open();
                 return conectar;
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 
-
-        public static void meteConsulta(DataGridView dgvControl, MySqlConnection conexion, string consulta)
-        {
-            
-            DataSet dsConsulta;
-            MySqlDataAdapter daConsulta;
-            try
-            {
-                daConsulta = new MySqlDataAdapter(consulta, conexion);
-
-                dsConsulta = new DataSet();
-                daConsulta.Fill(dsConsulta, "consulta");
-                dgvControl.DataSource = dsConsulta;
-                dgvControl.DataMember = "consulta";
-             
-
-            }
-            catch (Exception exception)
-            {
-                MessageBox.Show("Error al mostrar datos :" + exception.Message);
-            }
-
-=======
-=======
-
->>>>>>> origin/master
         public static void meteConsulta(DataGridView dgvControl, MySqlConnection conexion, string consulta)
         {
             DataSet dsConsulta;
@@ -61,7 +33,6 @@ namespace PracticaFinalGrupo3
             daConsulta.Fill(dsConsulta, "consulta");
             dgvControl.DataSource = dsConsulta;
             dgvControl.DataMember = "consulta";
->>>>>>> origin/master
         }
 
     }
